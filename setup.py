614d5754d4927d7f17c7ece452e521d1bef84395
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-13 13:16:52 +0900

    Update setup.py to upload this package to pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-transparent-centralwidget-window',
-    version='0.1.0',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
@@ -11,9 +18,11 @@ setup(
                 'as main widget and make central widget of QMainWindow transparent. '
                 'Simply put, frame with no image',
     url='https://github.com/yjg30737/pyqt-transparent-centralwidget-window.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.15',
-        'pyqt-custom-titlebar-window @ git+https://git@github.com/yjg30737/pyqt-custom-titlebar-window.git@main',
-        'pyqt-top-titlebar-widget @ git+https://git@github.com/yjg30737/pyqt-top-titlebar-widget.git@main'
+        'pyqt-custom-titlebar-window>=0.0.1',
+        'pyqt-top-titlebar-widget>=0.0.1'
     ]
 )
